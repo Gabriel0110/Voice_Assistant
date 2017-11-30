bd5f6d221c1feef2514754739d6e7ca850a918ca
Gabriel Tomberlin <gabrieltomberlin14@gmail.com>
2017-11-30 15:49:23 -0500

    Update Voice_AI.py
    Large change with new commands and infinite running loop of "Jarvis".

--- Voice_AI.py
+++ Voice_AI.py
@@ -3,26 +3,30 @@ import win32com.client as wincl
 import subprocess
 import webbrowser
 import time
+import re
 
 
 def main():
+	voice_ai()
 
-	prompt = input("To activate the AI, first press T and then enter.\n")
-
-	if prompt == "T" or prompt == "t":
-		time.sleep(1.5)
-		voice_ai()
-	else:
-		time.sleep(2.5)
-		print("That command is not recognized!")
-		print("Exiting.")
-		exit()
+def findWholeWord(w):
+	return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
+	# findWholeWord allows for easier recognition of certain commands, making
+	# it act just like Amazon's Alexa. If it hears a certain command, like
+	# "goodbye jarvis" in a sentence, it will execute command. With Alexa,
+	# if you say Alexa in a sentence while talking to someone, she activates.
+	# This could be more or less annoying, so it is just preference to user.
+	# It can be changed if desired, just don't use the function.
+	
+def googleSearch(query):
+	chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+	address = 'http://www.google.com/#q='
+	search = address + query
+	webbrowser.get(chrome_path).open(search)
 		
 def voice_ai():
 	# create TTS (text-to-speech) voice
 	speak = wincl.Dispatch("SAPI.SpVoice")
-	
-	print("Hello! What can I help you with?")
 	
 	# recognize speech using Google Speech Recognition
 	while True:
@@ -30,24 +34,48 @@ def voice_ai():
 		r = sr.Recognizer()
 		with sr.Microphone() as source:
 			audio = r.listen(source)
+			
+		result = r.recognize_google(audio)
 		
 		try:
-			if r.recognize_google(audio) == "goodbye":
-				speak.Speak("Goodbye!")
-				exit()
-			elif r.recognize_google(audio) == "open epic games":
-				speak.Speak("Opening Epic Games Launcher")
-				subprocess.call(['C:\Program Files (x86)\Epic Games\Launcher\Portal\Binaries\Win64\EpicGamesLauncher.exe'])
-			elif r.recognize_google(audio).lower() == "open youtube":
-				speak.Speak("Opening YouTube on your browser")
-				webbrowser.open("https://www.youtube.com/")
-			else:
-				speak.Speak("I do not recognize that command")
-				print("Google Speech Recognition thinks you said \"" + r.recognize_google(audio) + "\"")
+			if str(findWholeWord('hey jarvis')(result)) != "None":
+				active = True
+				speak.Speak("How can I help you?") # just to know it activates, can remove later
+				
+				while active:
+					# obtain audio from the microphone
+					r = sr.Recognizer()
+					with sr.Microphone() as source:
+						audio = r.listen(source)
+						
+					result = r.recognize_google(audio)
+					
+					# COMMAND OPTIONS
+					try:
+						if str(findWholeWord('goodbye jarvis')(result.lower())) != "None":
+							speak.Speak("Goodbye!")
+							active = False
+						elif str(findWholeWord('open epic games')(result)) != "None":
+							speak.Speak("Opening Epic Games Launcher")
+							subprocess.call(['C:\Program Files (x86)\Epic Games\Launcher\Portal\Binaries\Win64\EpicGamesLauncher.exe'])
+							active = False
+						elif result.lower() == "open youtube": # open youtube on your browser
+							speak.Speak("Opening YouTube on your browser")
+							webbrowser.open("https://www.youtube.com/")
+							active = False
+						elif result.startswith("search"): # search for anything on Google
+							speak.Speak("Searching for " + result[7:] + " on your browser")
+							googleSearch(result[7:])
+							active = False
+						else:
+							speak.Speak("I do not recognize that command, please try again.")
+							print("Google Speech Recognition thinks you said \"" + result + "\"")
+					except sr.UnknownValueError:
+						print("Google Speech Recognition could not understand audio")
+					except sr.RequestError as e:
+						print("Could not request results from Google Speech Recognition service; {0}".format(e))
 		except sr.UnknownValueError:
-			print("Google Speech Recognition could not understand audio")
-		except sr.RequestError as e:
-			print("Could not request results from Google Speech Recognition service; {0}".format(e))
+						print("Google Speech Recognition could not understand audio")
 			
 if __name__ == "__main__":
     main()		
