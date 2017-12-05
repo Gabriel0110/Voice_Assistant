fa254cd88ce3429bfa85b272498f03bfe8962289
Gabriel Tomberlin <gabrieltomberlin14@gmail.com>
2017-12-05 00:01:48 -0500

    Update play_game_command.py
    Fix indentation and add kill process command

--- play_game_command.py
+++ play_game_command.py
@@ -7,11 +7,13 @@ def chooseGame(game.lower()):
 		
 def playAvolition():
 	p = subprocess.Popen(['C:\WINDOWS\system32\cmd.exe'])
-		pyautogui.typewrite("cd ..")
-		pyautogui.press("enter")
-		pyautogui.typewrite("cd ..")
-		pyautogui.press("enter")
-		pyautogui.typewrite("cd Avolition")
-		pyautogui.press("enter")
-		pyautogui.typewrite("python main.py")
-		pyautogui.press("enter")
+	
+	pyautogui.typewrite("cd ..")
+	pyautogui.press("enter")
+	pyautogui.typewrite("cd ..")
+	pyautogui.press("enter")
+	pyautogui.typewrite("cd Avolition")
+	pyautogui.press("enter")
+	pyautogui.typewrite("python main.py")
+	pyautogui.press("enter")
+	p.kill()
