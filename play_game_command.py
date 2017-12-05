5a599e48f505d3f4bb6c6bb821a1894ee3824853
Gabriel Tomberlin <gabrieltomberlin14@gmail.com>
2017-12-04 23:59:02 -0500

    Create play_game_command.py
    Needed module for "play game" voice command

---
+++ play_game_command.py
@@ -0,0 +1,17 @@
+import subprocess
+import pyautogui
+
+def chooseGame(game.lower()):
+	if game == "avolition":
+		playAvolition()
+		
+def playAvolition():
+	p = subprocess.Popen(['C:\WINDOWS\system32\cmd.exe'])
+		pyautogui.typewrite("cd ..")
+		pyautogui.press("enter")
+		pyautogui.typewrite("cd ..")
+		pyautogui.press("enter")
+		pyautogui.typewrite("cd Avolition")
+		pyautogui.press("enter")
+		pyautogui.typewrite("python main.py")
+		pyautogui.press("enter")
