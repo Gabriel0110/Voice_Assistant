9478c9ea586146129e16cf5024a681ae5404ce54
Gabriel Tomberlin <gabrieltomberlin14@gmail.com>
2017-12-05 10:23:54 -0500

    Update work_timer.py
    Fixed bug. Also commented out the loop. I have to figure out how to run timer loop while running Jarvis behind scenes. For now, you just have to ask Jarvis to start timer every time you want a timer.

--- work_timer.py
+++ work_timer.py
@@ -2,15 +2,23 @@ import time
 import pyautogui
 import win32com.client as wincl
 
-def workTimer():
+def workTimer(limit):
 	speak = wincl.Dispatch("SAPI.SpVoice")
 	
-	check = confirm(text = "TAKE A 15 MINUTE BREAK", title = "Work/Study Break Timer", buttons = ['OK', 'No thanks'])
+	check = pyautogui.confirm(text = "TAKE A " + str(limit) + " MINUTE BREAK", title = "Work/Study Break Timer", buttons = ['OK', 'No thanks'])
 	
-	while check == "OK":
-		speak.Speak("Timer started for 15 minutes")
-		time.sleep(15 * 60)
+	if check == "OK":
+		speak.Speak("Timer started for " + str(limit) + " minutes")
+		time.sleep(limit * 60)
 		speak.Speak("Break time has ended")
-		alert(text = "Break over, get back to work! (1 Hour)", title = "Work/Study Break Timer", buttons = ['OK'])
+		pyautogui.alert(text = "Break over, get back to work! (1 Hour)", title = "Work/Study Break Timer", button = ['OK'])
+	
+	"""
+	while check == "OK":
+		speak.Speak("Timer started for " + str(limit) + " minutes")
+		time.sleep(limit * 60)
+		speak.Speak("Break time has ended")
+		pyautogui.alert(text = "Break over, get back to work! (1 Hour)", title = "Work/Study Break Timer", button = ['OK'])
 		time.sleep(60 * 60)
-		check = confirm(text = "TAKE A 15 MINUTE BREAK", title = "Work/Study Break Timer", buttons = ['OK', 'No thanks'])
+		check = pyautogui.confirm(text = "TAKE A " + str(limit) + " MINUTE BREAK", title = "Work/Study Break Timer", buttons = ['OK', 'No thanks'])
+	"""
